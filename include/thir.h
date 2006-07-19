9d27cf3c4ba8e767e78b5d0cf42dc1a0d75bea85
Brendan Jurd <direvus@gmail.com>
2006-07-19 08:38:03 +0000

    Reorganised the numbering of my window IDs to improve extensibility, and added in IDs for the dice counters and remaining dice bitmaps.

--- include/thir.h
+++ include/thir.h
@@ -1,10 +1,27 @@
-#define IDC_D2		102
-#define IDC_D4		104
-#define IDC_D6		106
-#define IDC_D8		108
-#define IDC_D10		110
-#define IDC_D12		112
-#define IDC_D20		120
-#define IDC_D100	200
+#define IDC_D2		1002
+#define IDC_D4		1004
+#define IDC_D6		1006
+#define IDC_D8		1008
+#define IDC_D10		1010
+#define IDC_D12		1012
+#define IDC_D20		1020
+#define IDC_D100	1100
 
-#define IDB_D4		1004
+#define IDB_D2		2002
+#define IDB_D4		2004
+#define IDB_D6		2006
+#define IDB_D8		2008
+#define IDB_D10		2010
+#define IDB_D12		2012
+#define IDB_D20		2020
+#define IDB_D100	2100
+
+#define IDC_ND2		3002
+#define IDC_ND4		3004
+#define IDC_ND6		3006
+#define IDC_ND8		3008
+#define IDC_ND10	3010
+#define IDC_ND12	3012
+#define IDC_ND20	3020
+#define IDC_ND100	3100
+
