5bd1de64c45750caed6dc399e07b8c9928c58a75
Brendan Jurd <direvus@gmail.com>
2010-07-19 22:17:51 +1000

    Remove the pov-ray images and the rules to build them.
    The pov-ray images are no longer used in the application, so get them
    out of the way.

--- images/Rules.mk
+++ images/Rules.mk
@@ -1,24 +1,13 @@
-RT = "povray"
-RTFLAGS = -D +W32 +H24 +A +Q11
 SVG = "inkscape"
 SVGFLAGS = -z
 
-POVS = $(wildcard images/*.pov)
 SVGS = $(wildcard images/*.svg)
 ICOS = images/app.ico
-POVBMPS = $(POVS:.pov=.bmp)
-POVPNGS = $(POVS:.pov=.png)
 SVGPNGS = $(SVGS:.svg=.png)
 
 SHARE += $(SVGPNGS)
 FILES += $(SVGPNGS)
-CLEAN += $(POVBMPS) $(POVPNGS) $(SVGPNGS)
-
-$(POVBMPS): %.bmp: %.pov
-	$(RT) $(RTFLAGS) +FS +O- $< > $@
-
-$(POVPNGS): %.png: %.pov
-	$(RT) $(RTFLAGS) +FN +UA +O- $< > $@
+CLEAN += $(SVGPNGS)
 
 $(SVGPNGS): %.png: %.svg
 	$(SVG) $(SVGFLAGS) -e $@ $<
