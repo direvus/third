1c4ba711289a64f6d843fd1181b46983c2975ebe
Brendan Jurd <direvus@gmail.com>
2010-07-20 07:40:18 +1000

    Add a new third Android project.
    At this stage the project is just a 'Hello, world' proof of concept app.

---
+++ android/src/au/id/swords/third/ThirdActivity.java
@@ -0,0 +1,16 @@
+package au.id.swords.third;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ThirdActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
