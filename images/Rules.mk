7f2eb58355f52c6efb9fbfa6eeb2f63bcdb7d6db
Brendan Jurd <direvus@gmail.com>
2010-07-20 10:00:02 +1000

    Add rules to build a 48px version of the app icon.

--- images/Rules.mk
+++ images/Rules.mk
@@ -4,6 +4,7 @@ SVGFLAGS = -z
 SVGS = $(wildcard images/*.svg)
 ICOS = images/app.ico
 SVGPNGS = $(SVGS:.svg=.png)
+SVGPNGS_48 = $(SVGS:.svg=_48.png)
 
 SHARE += $(SVGPNGS)
 FILES += $(SVGPNGS)
@@ -11,6 +12,9 @@ CLEAN += $(SVGPNGS)
 
 $(SVGPNGS): %.png: %.svg
 	$(SVG) $(SVGFLAGS) -e $@ $<
+
+$(SVGPNGS_48): %_48.png: %.svg
+	$(SVG) $(SVGFLAGS) -w 48 -h 48 -e $@ $<
 
 $(ICOS): %.ico: %.png
 	icotool -c -o $@ $<
