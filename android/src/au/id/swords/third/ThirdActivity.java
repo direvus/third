e64ac6bf55ea39665dc17fd5d2a071f72ee5c7a4
Brendan Jurd <direvus@gmail.com>
2010-07-21 17:55:33 +1000

    Get the basic layout working with composite widgets.
    I have used a subclass of TableRow to represent the composite
    arrangement of a dice button with its companion counter field.  The idea
    is for the button to be capable of controlling the counter; clicking
    on the button should advance the counter by one each time.  I haven't
    yet built any of the listeners to make this work; this commit just
    establishes the layout.

--- android/src/au/id/swords/third/ThirdActivity.java
+++ android/src/au/id/swords/third/ThirdActivity.java
@@ -2,7 +2,16 @@ package au.id.swords.third;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Context;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TableRow;
+import android.widget.TableLayout;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.LayoutInflater;
+import java.lang.reflect.Field;
 
 public class ThirdActivity extends Activity
 {
@@ -12,5 +21,35 @@ public class ThirdActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        TableLayout t = (TableLayout)findViewById(R.id.counters);
+        t.addView(new Counter(this, R.drawable.d2,   "d2"));
+        t.addView(new Counter(this, R.drawable.d4,   "d4"));
+        t.addView(new Counter(this, R.drawable.d8,   "d8"));
+        t.addView(new Counter(this, R.drawable.d10,  "d10"));
+        t.addView(new Counter(this, R.drawable.d12,  "d12"));
+        t.addView(new Counter(this, R.drawable.d20,  "d20"));
+        t.addView(new Counter(this, R.drawable.d100, "d100"));
+        t.addView(new Counter(this, R.drawable.mul,  "mul"));
+        t.addView(new Counter(this, R.drawable.mod,  "mod"));
+    }
+
+    private class Counter extends TableRow
+    {
+        ImageButton button;
+        EditText counter;
+
+        public Counter(Context ctx, int image, String name)
+        {
+            super(ctx);
+
+            LayoutInflater li;
+            li = (LayoutInflater)ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
+            li.inflate(R.layout.counter, this);
+
+            button = (ImageButton)findViewById(R.id.button);
+            button.setImageResource(image);
+            counter = (EditText)findViewById(R.id.counter);
+        }
     }
 }
